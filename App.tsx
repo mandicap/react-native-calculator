79edd1d0257ebd34faedf2ad2b48c98e3b974565
Amanda Romero <mandicap@comcast.net>
2021-10-12 23:34:35 -0600

    Adds output container

--- App.tsx
+++ App.tsx
@@ -10,39 +10,42 @@ const App = () => {
         backgroundColor="#61dafb"
         barStyle="light-content" />
 
-      <View style={styles.container}>
-        <Row>
-          <Button text="7" onPress={() => ''} />
-          <Button text="8" onPress={() => ''} />
-          <Button text="9" onPress={() => ''} />
-          <Button text="÷" onPress={() => ''} theme="secondary" />
-        </Row>
-
-        <Row>
-          <Button text="4" onPress={() => ''} />
-          <Button text="5" onPress={() => ''} />
-          <Button text="6" onPress={() => ''} />
-          <Button text="×" onPress={() => ''} theme="secondary" />
-        </Row>
-
-        <Row>
-          <Button text="1" onPress={() => ''} />
-          <Button text="2" onPress={() => ''} />
-          <Button text="3" onPress={() => ''} />
-          <Button text="-" onPress={() => ''} theme="secondary" />
-        </Row>
-
-        <Row>
-          <Button text="0" onPress={() => ''} />
-          <Button text="." onPress={() => ''} />
-          <Button text="⌫" onPress={() => ''} />
-          <Button text="+" onPress={() => ''} theme="secondary" />
-        </Row>
-
-        <Row>
-          <Button text="=" onPress={() => ''} theme="accent" />
-        </Row>
+      <View style={styles.outputContainer}>
+        <Text style={styles.outputText}>
+        </Text>
       </View>
+
+      <Row>
+        <Button text="7" onPress={() => ''} />
+        <Button text="8" onPress={() => ''} />
+        <Button text="9" onPress={() => ''} />
+        <Button text="÷" onPress={() => ''} theme="secondary" />
+      </Row>
+
+      <Row>
+        <Button text="4" onPress={() => ''} />
+        <Button text="5" onPress={() => ''} />
+        <Button text="6" onPress={() => ''} />
+        <Button text="×" onPress={() => ''} theme="secondary" />
+      </Row>
+
+      <Row>
+        <Button text="1" onPress={() => ''} />
+        <Button text="2" onPress={() => ''} />
+        <Button text="3" onPress={() => ''} />
+        <Button text="-" onPress={() => ''} theme="secondary" />
+      </Row>
+
+      <Row>
+        <Button text="0" onPress={() => ''} />
+        <Button text="." onPress={() => ''} />
+        <Button text="⌫" onPress={() => ''} />
+        <Button text="+" onPress={() => ''} theme="secondary" />
+      </Row>
+
+      <Row>
+        <Button text="=" onPress={() => ''} theme="accent" />
+      </Row>
     </SafeAreaView>
   );
 }
@@ -54,6 +57,16 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  outputContainer: {
+    flex: 1,
+    alignItems: 'flex-end',
+    justifyContent: 'flex-end',
+    width: '100%',
+    padding: 15
+  },
+  outputText: {
+    fontSize: 50
+  }
 });
 
 export default App;
